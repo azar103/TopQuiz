532cfe12e79b1ed387d49c92feba580ad3801553
azar103 <azzarroukanis@gmail.com>
2019-01-31 15:06:15 +0100

    premiere jour du premier tuto

---
+++ app/src/main/java/com/zarrouk/anis/topquiz/MainActivity.java
@@ -0,0 +1,50 @@
+package com.zarrouk.anis.topquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mGreetingText;
+    private EditText mNameInput;
+    private Button   mPlayBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mGreetingText = (TextView) findViewById(R.id.activity_main_greeting_txt);
+        mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
+        mPlayBtn = (Button) findViewById(R.id.activity_main_play_btn);
+        mPlayBtn.setEnabled(false);
+        mNameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+
+                    mPlayBtn.setEnabled(s.toString().length()!= 0);
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        mPlayBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
